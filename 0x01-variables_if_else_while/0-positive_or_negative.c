b031189139a8e69ee5769a24c5af69f94170b85a
Heaven002 <heavenatlast66@.com>
2022-06-05 09:55:25 -0700

    using variables, if and else if statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,7 +18,7 @@ int main(void)
 	if (n > 0)
 		printf("%i is positive\n", n);
 	else if (n < 0)
-		printf("% is negtive\n", n);
+		printf("%i is negative\n", n);
 	else
 		printf("%i is zero", n);
 	return (0);
